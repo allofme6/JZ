a17969ca70002d95da1a3499434f41e58caa82ee
changhao <153067703@qq.com>
2019-10-11 17:20:23 +0800

    profile-设置-changhao

---
+++ src/pages/index/profile/components/setup/StyeldSetup.js
@@ -0,0 +1,13 @@
+import styled from 'styled-components'
+
+const SetupContainer = styled.div`
+    width: 100%;
+    height: 100%;
+    .setup-list{
+        width: 100%;
+    }
+`
+
+export{
+    SetupContainer
+}
