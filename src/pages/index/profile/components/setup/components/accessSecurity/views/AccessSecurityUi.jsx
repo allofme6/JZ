18d7d753b6377826372f5ab90488643657cf6db4
allofme6 <798266304@qq.com>
2019-10-15 08:10:04 +0800

    test

--- src/pages/index/profile/components/setup/components/accessSecurity/views/AccessSecurityUi.jsx
+++ src/pages/index/profile/components/setup/components/accessSecurity/views/AccessSecurityUi.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 import NavBar from 'components/navBar/NavBar'
 import SetItem from 'components/setItem/SetItem'
-import { Modal, Button } from 'antd-mobile'
 import { OptionStyle } from './AccessSecurityStyle'
+import App from 'components/modal/Modal'
 
 export default (props) => {
     return (
@@ -13,8 +13,7 @@ export default (props) => {
             />
             <OptionStyle>
                 <SetItem 
-                    title={<App question="是否修改手机号" title="修改手机号"></App>}
-                    handleActions={() => {showAlert(props.question)}}
+                    title={<App name="jahah" question="是否修改手机号" title="修改手机号"></App>}
                 >
                 </SetItem>
                 <SetItem 
@@ -24,21 +23,3 @@ export default (props) => {
         </div>
     )
 }
-
-const alert = Modal.alert;
-
-const showAlert = (question) => {
-  const alertInstance = alert('', question , [
-    { text: '不保存', onPress: () => console.log('cancel'), style: 'default' },
-    { text: '保存', onPress: () => console.log('ok') },
-  ]);
-  setTimeout(() => {
-    // 可以调用close方法以在外部close
-    console.log('auto close');
-    alertInstance.close();
-  }, 500000);
-};
-
-const App = (props) => (
-    <Button onClick={() => showAlert(props.question)}>{props.title}</Button>
-)
