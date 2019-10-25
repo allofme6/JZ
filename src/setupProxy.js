f1ac1015af8982bca141eb59d65bb5761b12a31b
allofme6 <798266304@qq.com>
2019-10-25 10:44:09 +0800

    test

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -2,11 +2,7 @@ const proxy = require('http-proxy-middleware')
 
 module.exports = function(app) {
   app.use('/api', proxy({
-<<<<<<< HEAD
-    target: 'http://10.9.24.153:8080/',
-=======
     target: 'http://10.9.24.153:8080',
->>>>>>> 389ddb31286eb4b569b23c804f547a28bff0e3ab
     changeOrigin: true,
     pathRewrite:{
       '^/api': ''
